e90c7908cfadd42da4a28ff66eabcb75fa8b7b36
OlyaBakay <olhabak@gmail.com>
2017-06-17 18:37:13 +0300

    fixed vector

--- Vector.h
+++ Vector.h
@@ -4,14 +4,19 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <cassert>
+#include <cmath>
+#include "Vector.h"
+#include <thread>
+#include <mutex>
+#include <vector>
 
 template <class T>
 class Vector {
 
 private:
 
-    struct VectorMemory
-    {
+    struct VectorMemory {
         T *data;
         size_t cols;
 
@@ -20,12 +25,10 @@ private:
             data = new T[cols];
         }
 
-
         ~VectorMemory() // RAII
         {
             delete[] data;
         }
-
 
         inline T &operator()(size_t i)
         {
@@ -37,7 +40,43 @@ private:
             return *(data + i);
         }
 
-        void bf() const {}
+        VectorMemory (const VectorMemory& other):
+        cols(other.cols)
+        {
+            data = new T[other.cols];
+            std::copy(other.data, other.data + cols, data);
+        }
+
+        static void swap(VectorMemory & a, VectorMemory & b){
+            std::swap(a.data, b.data);
+            std::swap(a.cols, b.cols);
+        }
+
+
+        VectorMemory& operator= (const VectorMemory& b)
+        {
+            VectorMemory temp(b);
+            VectorMemory::swap(temp, *this);
+            return *this;
+        }
+
+//    Vector(const Vector<T>& other) : vec_data(other.cols())
+//    {
+//        copy(other.vec_data.data, other.vec_data.data + vec_data.cols, vec_data.data);
+//    }
+//
+//        VectorMemory (const VectorMemory<T>& other):
+//                vec_data(other.cols())
+//        {
+//            copy(other.vec_data.data, other.vec_data.data + vec_data.cols, vec_data.data);
+//        }
+//
+////                cols(other.cols)
+////        {
+////            data = new T[other.cols];
+////            copy(other.vect_data.data, other.data + cols, data);      //???????????????
+////
+////        }
 
     } vec_data;
 
@@ -45,32 +84,33 @@ public:
 
     Vector(size_t c) : vec_data(c) {}
 
-    Vector(const Vector<T>& other) : vec_data(other.cols())
-    {
-        copy(other.vec_data.data, other.vec_data.data + vec_data.cols, vec_data.data);
-    }
 
-    void operator = (T* arr[])
+
+    Vector(std::initializer_list<T> ilst) : vec_data(std::sqrt(ilst.size()))
     {
-         std::copy(vec_data.data, vec_data.data + cols(), arr);
-//        for (int i = 0; i < vec_data.cols; i ++)
-//        {
-//            vec_data.data[i] = arr[i];
-//        }
+        assert(std::sqrt(ilst.size())==ilst.size());
+        for (auto i = begin(ilst); i != end(ilst); i++)
+        {
+            vec_data.data[i - begin(ilst)] = *i;
+        }
 
     }
-    void operator = (std::initializer_list<T> ilst)
+
+    Vector& operator = (std::initializer_list<T> ilst)
     {
         if (ilst.size() != vec_data.cols)
         {
-            throw std::out_of_range("Too big arr to initialize vector with");
+            throw std::out_of_range("Too big lst to initialize vector with");
         }
 
         for (auto i = begin(ilst); i != end(ilst); i++)
         {
             vec_data.data[i - begin(ilst)] = *i;
         }
+        //std::copy(matr_data.data, matr_data.data + (matr_data.rows * matr_data.cols), begin(ilst));
+        return *this;
     }
+
 
     size_t cols() const
     {
@@ -82,19 +122,11 @@ public:
         return vec_data.data;
     }
 
-    Vector& operator = (const Vector<T>& other)
-    {
-        vec_data = VectorMemory(other.vec_data.cols);
-        copy(other.vec_data.data, other.vec_data.data + vec_data.cols, vec_data.data);
-        return *this;
-    }
-
     inline T& operator()(size_t i) {
         if (i >= vec_data.cols || i < 0) {
             throw std::out_of_range ("Vector index out of range");
         }
         return vec_data(i);
-
     }
 
     inline const T& operator()(size_t i) const {
@@ -116,35 +148,23 @@ public:
         return out;
     }
 
-    double operator*(const Vector &other){
-        const Vector &self = *this;
-        double res = 0;
-        for (size_t i = 0; i < this->cols(); i++) {
-            res += self(i) * other(i);
-        }
-        return res;
-    }
-
-    double operator*=(const Vector &other){
-        const Vector &self = *this;
-        return self*other;
-    }
-
     Vector& operator+=(const Vector &other)
     {
         assert(cols()==other.cols());
         Vector &self = *this;
+
         for (size_t i = 0; i < this->cols(); i++) {
-             self(i) += other(i);
+            self(i) += other(i);
         }
-        return *this;  //memory saving?
+        return *this;
     }
 
-    Vector operator-=(const Vector &other) {
+    Vector& operator-=(const Vector &other) {
+        assert(cols() == other.cols());
         Vector &self = *this;
 
         for (size_t i = 0; i < this->cols(); i++) {
-             self(i) -= other(i);
+            self(i) -= other(i);
         }
         return *this;
     }
@@ -154,7 +174,7 @@ public:
         Vector &self = *this;
         for (size_t i = 0; i < cols(); i++) {
             self(i) = self(i) + scalar;
-            }
+        }
 
         return self;
     }
@@ -163,23 +183,44 @@ public:
     {
         Vector &self = *this;
         for (size_t i = 0; i < cols(); i++) {
-                self(i) = self(i) - scalar;
+            self(i) = self(i) - scalar;
         }
         return self;
+    }
+
+
+    float_t operator*(const Vector &other){
+        const Vector &self = *this;
+        float_t res_n = 0;
+
+        for (size_t i = 0; i < cols(); i++) {
+            res_n += self(i) * other(i);
+        }
+
+        return res_n;
+    }
+
+    Vector& operator*=(const T &scalar)
+    {
+        Vector &self = *this;
+        for (size_t i=0; i < cols(); i++){
+            self(i) = self(i) *scalar;
+        }
+        return self;
+    }
+    Vector &operator- ()
+    {
+        Vector &vect =*this;
+
+            for (size_t i=0; i<cols(); i++){
+                vect(i) =  - vect(i);
+            }
+        return vect;
     }
 
     Vector &operator+ ()
     {
         return *this;
-    }
-
-    Vector &operator- ()
-    {
-        Vector &vect =*this;
-            for (size_t i=0; i<cols(); i++){
-                vect(i) =  - vect(i);
-            }
-        return vect;
     }
 
 };
@@ -191,13 +232,19 @@ inline Vector<T> operator+(Vector<T> left, const Vector<T> &other) {
 }
 
 template<typename T>
-inline Vector<T> operator+(const T &scalar, Vector<T> other) {
-    return other += scalar;
+inline Vector<T> operator+(const T &scalar, Vector<T> right) {
+    return right += scalar;
 }
 
 template<typename T>
-inline Vector<T> operator+(Vector<T> vec, const T &scalar){
-    return vec += scalar;
+inline Vector<T> operator+(Vector<T> left, const T &scalar){
+    return left += scalar;
+}
+
+template<typename T>
+inline Vector<T> operator- (Vector<T> left, const Vector<T> &other){
+    assert(left.cols() == other.cols());
+    return left -= other;
 }
 
 template<typename T>
@@ -206,14 +253,18 @@ inline Vector<T> operator- (Vector<T> vec, const T &scalar) {
 }
 
 template<typename T>
-inline Vector<T> operator- (Vector<T> left, const Vector<T> other){
-    assert(left.cols() == other.cols());
-    return left -= other;
+inline Vector<T> operator- (const T &scalar, Vector<T> right) {
+    return -right += scalar;
 }
 
 template<typename T>
-inline Vector<T> operator- (const T &scalar, Vector<T> right) {
-    return -right += scalar;
+inline Vector<T> operator*(Vector<T> left, const T& scalar){
+    return left *= scalar;
+}
+
+template<typename T>
+inline Vector<T> operator*(const T& scalar, Vector<T> right){
+    return right *= scalar;
 }
 
 
