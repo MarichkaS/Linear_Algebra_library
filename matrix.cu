d75c32c1814e833b833c87be3848d0576da9a07f
programminggirl <dobko_m@ucu.edu.ua>
2017-06-20 10:02:00 +0300

    added to cuda

--- matrix.cu
+++ matrix.cu
@@ -28,6 +28,15 @@ __global__ void Matrix_Addition ( const int *dev_a , const int *dev_b , int *dev
 
 }
 
+__global__ void Matrix_Subtraction ( const int *dev_a , const int *dev_b , int *dev_c)
+{
+    //Get the id of thread within a block
+    unsigned short tid = threadIdx.x ;
+
+    if ( tid < N ) // check the boundry condition for the threads
+        dev_c [tid] = dev_a[tid] - dev_b[tid] ;
+
+}
 
 int main (void)
 {
@@ -64,14 +73,14 @@ int main (void)
     HANDLE_ERROR (cudaMemcpy (dev_b , Host_b , N*sizeof(int) , cudaMemcpyHostToDevice));
 
     //Make a call to GPU kernel
-    Matrix_Addition <<< 1, N  >>> (dev_a , dev_b , dev_c ) ;
+    Matrix_Subtraction <<< 1, N  >>> (dev_a , dev_b , dev_c ) ;
 
     //Copy back to Host array from Device array
     HANDLE_ERROR (cudaMemcpy(Host_c , dev_c , N*sizeof(int) , cudaMemcpyDeviceToHost));
 
     for ( int i = 0; i<N; i++ )
         printf ("%d\n", Host_c[i] ) ;
-
+   
     //Free the Device array memory
     cudaFree (dev_a) ;
     cudaFree (dev_b) ;
